65a29bce0df7727538d4c813e3331238be20914e
kmou424 <dx2689282850@gmail.com>
2021-03-17 18:38:06 +0800

    oneindex: install2: Fix a typo
    Signed-off-by: kmou424 <dx2689282850@gmail.com>

--- view/admin/install/install_2.php
+++ view/admin/install/install_2.php
@@ -7,7 +7,7 @@
 	  <h1>程序安装 <small>绑定微软账号</small></h1>
 	</div>
 
-
+    <form>
 	 <a class="mdui-btn mdui-btn-raised mdui-float-left" href="?step=1">上一步</a>
 	 <a href="<?php echo onedrive::authorize_url();?>" class="mdui-btn mdui-color-theme-accent mdui-ripple mdui-float-right"><i class="mdui-icon material-icons">&#xeb3d;</i> 绑定账号</a>
       
