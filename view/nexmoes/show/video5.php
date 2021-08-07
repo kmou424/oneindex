6a2a9fe7a3679d0a760dc39e9e471146f6f8dfee
kmou424 <me@kmou424.moe>
2021-08-08 01:02:56 +0800

    [theme]nexmoes: Better video5 UI

--- view/nexmoes/show/video5.php
+++ view/nexmoes/show/video5.php
@@ -7,8 +7,19 @@
 		<video class="mdui-video-fluid mdui-center" preload controls poster="<?php @e($item['thumb']);?>">
 		  <source src="<?php e($item['downloadUrl']);?>" type="video/mp4">
 		</video>
+		<br>
+		<button class="mdui-btn mdui-btn-raised mdui-ripple mdui-color-pink-accent" onclick="location.href='<?php e($item['downloadUrl']);?>'" type="button" style="display:block;margin:0 auto">下载</button>
 	</div>
 	<br>
+	<div class="nexmoe-item">
+		<div class="mdui-textfield">
+		  <label class="mdui-textfield-label">下载地址</label>
+		  <input class="mdui-textfield-input" type="text" value="<?php e($url);?>"/>
+		</div>
+		<div class="mdui-textfield">
+		  <label class="mdui-textfield-label">引用地址</label>
+		  <textarea class="mdui-textfield-input"><video><source src="<?php e($url);?>" type="video/mp4"></video></textarea>
+		</div>
+	</div>
 </div>
-<a href="<?php e($url);?>" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-pink-accent"><i class="mdui-icon material-icons">file_download</i></a>
 <?php view::end('content');?>
