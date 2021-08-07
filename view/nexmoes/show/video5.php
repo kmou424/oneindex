80c6e9de63b9fbf5f5ecdb75abf0b3a6ebd4c9ae
kmou424 <me@kmou424.moe>
2021-08-07 22:01:49 +0800

    [theme]nexmoes: Add margin top&bottom for video5 preview

--- view/nexmoes/show/video5.php
+++ view/nexmoes/show/video5.php
@@ -2,11 +2,13 @@
 <?php $item['thumb'] = onedrive::thumbnail($item['path']);?>
 <?php view::begin('content');?>
 <div class="mdui-container-fluid">
+	<br>
 	<div class="nexmoe-item">
 		<video class="mdui-video-fluid mdui-center" preload controls poster="<?php @e($item['thumb']);?>">
 		  <source src="<?php e($item['downloadUrl']);?>" type="video/mp4">
 		</video>
 	</div>
+	<br>
 </div>
 <a href="<?php e($url);?>" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-pink-accent"><i class="mdui-icon material-icons">file_download</i></a>
 <?php view::end('content');?>
